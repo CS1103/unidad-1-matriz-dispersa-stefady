f72d5836028cd306999edd38364a94f6f758b6be
grace_joaquin8 <40775025+stefady@users.noreply.github.com>
2019-09-05 15:02:17 -0500

    Create Matriz.h

---
+++ Matriz.h
@@ -0,0 +1,48 @@
+
+// Created by HP on 3/09/2019.
+//
+
+#ifndef PRACTICAS_MATRIZ_MATRIZ_H
+#define PRACTICAS_MATRIZ_MATRIZ_H
+
+using TipoEspacio= long int;
+using TipoPorcentaje=float;
+using TipoCaracter=int;
+
+class Matriz {
+private:
+    TipoEspacio ALTO;
+    TipoEspacio ANCHO;
+    TipoPorcentaje dis;
+    TipoEspacio **M;
+    TipoEspacio *Value;
+    TipoEspacio *Ancho;
+    TipoEspacio *Alto;
+
+public:
+    Matriz();
+    Matriz(TipoEspacio alto,TipoEspacio ancho);
+    ~Matriz();
+
+    //TipoPorcentaje setdis(TipoPorcentaje _dis){dis=_dis;}
+    //TipoPorcentaje getdis(){return dis;}
+
+    float dispercion(TipoPorcentaje dis);
+
+    void valoresMatriz();
+    void crearespacio();
+    void imprimirMatriz();
+    void imprimirdatos();
+    int getAlto();
+    int getAncho();
+    void Suma(const Matriz &A,const Matriz &B);
+    void transpuesta(const Matriz &A);
+    void multiplicacionEscalar(const Matriz &A,int n);
+
+};
+
+void ejecutar();
+void Menu();
+
+
+#endif //PRACTICAS_MATRIZ_MATRIZ_H
